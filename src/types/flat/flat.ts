a6a27a0bd1295af10f11880eab1fa5c170dac069
toushik018 <tayebhossain018@gmail.com>
2024-07-02 20:41:57 +0600

    first commit

--- src/types/flat/flat.ts
+++ src/types/flat/flat.ts
@@ -1,26 +1,7 @@
 import { IMeta } from "../common";
-import { IUser } from "../user/user";
 
 
-export type IFlat = {
-  id: string;
-  location: string;
-  description: string;
-  rentAmount: number;
-  bedrooms: number;
-  amenities: string;
-  photos: string[];
-  requests: Request[];
-  postedBy: string;
-  createdAt: string;
-  updatedAt: string;
-  user: IUser;
-};
 
-export type IFlatResponse = {
-  data: IFlat[];
-  meta: IMeta;
-};
 
 export interface IRequest {
   id: string;
@@ -39,9 +20,6 @@ export interface IRequest {
 
 
 
-export interface FlatCardsClientProps {
-  initialFlats: IFlat[];
-}
 
 export interface SearchParams {
   location?: string;
